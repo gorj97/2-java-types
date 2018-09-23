ad8a224c7a39f155b8940612b3ab5f7a6f363528
gorj97 <gorj97@mail.ru>
2018-09-23 17:04:53 +0500

    Карпов Егор Задание №2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,26 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long numberInput = Long.parseLong(input);
+        if (numberInput >= Byte.MIN_VALUE && numberInput <= Byte.MAX_VALUE) return "byte";
+        else {
+            if (numberInput >= Short.MIN_VALUE && numberInput <= Short.MAX_VALUE) return "short";
+            else {
+                if (numberInput >= Integer.MIN_VALUE && numberInput <= Integer.MAX_VALUE) return "int";
+                else {
+                    return "long";
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
-        String result = solution("12345");
+        String result = solution("4000000000");
         System.out.println(result);
-         */
+        */
     }
 
 }
